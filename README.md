a920d6adc46ce4b7b0674de40c7b485f24eceeb1
geshengming <geshengming@geshengmingdeMacBook-Pro.local>
2019-12-20 10:51:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+### Create Nest App
+
+Easy to create a nest app
+
+### Usage
+``` bash
+npm install  geshengming@create-nest-app -g
+create-nest-app my-app
+```
+  
---
+++ index.js
@@ -0,0 +1,17 @@
+const commander = require('commander');
+const readlineSync = require('readline-sync');
+const program = new commander.Command();
+process.title = 'create-nest-app';
+program
+    .version(require('./package').version)
+
+program.parse(process.argv);
+console.log('your args: ', program.args);
+
+const db = [' MySQL ', ' MongoDB '];
+const index = readlineSync.keyInSelect(db, '选择项目内置的数据库');
+if (index === -1) {
+    console.error('项目创建已取消')
+} else {
+    console.log('Ok, ' + db[index] + ' goes to your room.');
+}
